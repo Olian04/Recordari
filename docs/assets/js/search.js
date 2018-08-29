5b77188f5e55556088a0cfe2df4802ff9e3d8cc0
Oliver <rift95@live.se>
2018-08-29 12:42:55 +0200

    docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"foo","url":"globals.html#foo","classes":"tsd-kind-function"}]};
