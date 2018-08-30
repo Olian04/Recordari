b4c270e2eb19eb5c64b418a174c8dafc3c99f89a
Oliver <rift95@live.se>
2018-08-30 14:35:15 +0200

    renamed things

--- src/lib/builder.spec.ts
+++ src/lib/builder.spec.ts
@@ -3,11 +3,11 @@ import { Builder } from './builder';
 import { internal, hasInternal, NodeType, IInternal } from './builder.interface';
 
 const child = (node: IInternal, depth: number): IInternal => depth === 0 ? node : child(node.children[0], depth-1);
-const assertChildType = (node: IInternal, childDepth: number, type: NodeType) => expect(child(node, childDepth).type).to.equal(type);
+const assertChild = (node: IInternal, childDepth: number, type: NodeType) => expect(child(node, childDepth).type).to.equal(type);
 const test = (name: string, constraint, cb: (internal: IInternal) => void) => {
   it(name, () => {
     if (hasInternal(constraint)) {
-      assertChildType(constraint[internal], 0, NodeType.Base);
+      assertChild(constraint[internal], 0, NodeType.Base);
       cb(constraint[internal].children[0]);
     } else {
       assert.fail();
@@ -17,14 +17,19 @@ const test = (name: string, constraint, cb: (internal: IInternal) => void) => {
 
 describe('builder', () => {
   test('Any', Builder.Any, a =>
-    assertChildType(a, 0, NodeType.Any)
+    assertChild(a, 0, NodeType.Any)
   );
   test('Number', Builder.Number, a =>
-    assertChildType(a, 0, NodeType.Number)
+    assertChild(a, 0, NodeType.Number)
   );
   test('and', Builder.and([ Builder.Number ]), a => {
-    assertChildType(a, 0, NodeType.And);
-    assertChildType(a, 1, NodeType.Base);
-    assertChildType(a, 2, NodeType.Number);
+    assertChild(a, 0, NodeType.And);
+    assertChild(a, 1, NodeType.Base);
+    assertChild(a, 2, NodeType.Number);
+  });
+  test('or', Builder.or([ Builder.Number ]), a => {
+    assertChild(a, 0, NodeType.Or);
+    assertChild(a, 1, NodeType.Base);
+    assertChild(a, 2, NodeType.Number);
   });
 });
