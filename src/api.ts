b1d0c10ffbcdaf96cf8eb0375e16382dfc0779e7
Oliver <rift95@live.se>
2018-09-07 13:04:16 +0200

    added v1.0.0 goals as comment

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,14 @@
 export { Builder as R } from './lib/builder';
+
 import { Evaluate } from './lib/evaluator';
+
+/*
+// This is the endgoal for v1.0.0
+// v2.0.0 should focus on bringing custom errors based on what failed.
+export const Record = (type: string, objectConstraint) => (obj: Object) => {
+  if (!Evaluate(obj, R.Object.Like(objectConstraint))) {
+    throw new Error(`Assertion for record instance of type ${type} failed!`);
+  }
+  return obj;
+}
+*/
