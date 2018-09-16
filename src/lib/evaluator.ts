855837bca9c08a1cb52b80906d6c4ec1c0326e13
Oliver Anteros <rift95@live.se>
2018-09-16 03:07:28 +0200

    Update evaluator.ts

--- src/lib/evaluator.ts
+++ src/lib/evaluator.ts
@@ -8,16 +8,18 @@ export const Evaluate = (value, root: INode): boolean => {
     if (type in predicates) {
       // Predicate
       return predicates[type](value, data) && (children[0] === undefined || _eval(value, children[0]));
-    } else if (type in extractions) {
+    } 
+    if (type in extractions) {
       // Extraction
       return children[0] === undefined || _eval(extractions[type](value, data), children[0]);
-    } else /* istanbul ignore else */ if (type in unique) {
+    } 
+    if (type in unique) {
       // Unique
       return unique[type](_eval, value, {type, data, children});
-    } else {
-      // This error should never be thrown if the tests pass, so the fact that it's not ran by the tests means that it's working as intended
-      throw new Error('Unexpected node type: ' + type);
     }
+    // This error should never be thrown if the tests pass, so the fact that it's not ran by the tests means that it's working as intended
+    /* istanbul ignore next */
+    throw new Error('Unexpected node type: ' + type);
   };
   return _eval(value, root[internal]);
 }
