6dbbd16ef6ceeaf7ea2c6f9904956e7c3103d8dc
Oliver <rift95@live.se>
2018-09-23 13:15:36 +0200

    added test for the usage example from the README

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -22,4 +22,51 @@ describe('api', () => {
       expect(e).to.be.an.instanceof(TypeError);
     }
   });
+  describe('Usage examples', () => {
+    it('Full example from README', () => {
+      const RSettings = Record('Settings', {
+        foo: R.Number.Natural,
+        bar: R.Array.Each.String.Either(['a', 'b']),
+        biz: R.Object.Like({
+          baz: R.Number.Between(0, 8)
+        }),
+        baz: R.or([
+          R.String.Length.Max(2),
+          R.String.Length.Min(10)
+        ]),
+        bez: R.and([
+          R.Regex.Test('1.1.0').True,
+          R.Regex.Test('0.0.0').False
+        ])
+      });
+
+      try {
+        const obj = {
+          foo: 5,
+          bar: ['b'],
+          biz: { baz: 1.654 },
+          baz: 'hi',
+          bez: /^(?!0\.0\.\d+$)\d+\.\d+\.\d+$/
+        };
+        const okRecord = RSettings(obj);
+        expect(okRecord).to.deep.equal(obj);
+      } catch {
+        expect.fail();
+      }
+
+      try {
+        const failRecord = RSettings({
+          foo: 5.01, //                     Error: Settings.foo => 5.01 is not a natural number
+          bar: ['c'], //                    Error: Settings.bar[0] => 'c' is not in ['a', 'b']
+          biz: { baz: 1.654, boo: 'd' }, // Error: Settings.biz => Unexpected key 'boo'.
+          baz: 'hello', //                  Error: Settings.baz => 'hello'.length is not, less than 3, nor greater than 9
+          hello: 'Record.js', //             Error: Settings => Unexpected key 'hello'.
+          bez: /^\d+\.\d+\.\d+$/ //         Error: Settings.bez => '0.0.0'.match should be false
+        });
+        expect.fail();
+      } catch {
+        expect(true).true;
+      }
+    });
+  });
 });
